dd6d1d6f3976ee6e3053338503bf5964c26b0fad
Victor Nnamani <93020611+NeonVictor651@users.noreply.github.com>
2022-09-22 11:39:19 +0100

    completed calculator with react

---
+++ src/app.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+
+
+export function Test() {
+
+  let [first,updateFirst] = useState(0);
+  let [second,updateSecond] = useState(0);
+  
+  return (
+  <div className="dialogholder">
+    <div className="dialog-box">
+      <div className="inputValues">
+        <input type="text" id="first" value={first} onChange={(evt) => {updateFirst(evt.target.value)}}/>
+        <i className="fa fa-plus" aria-hidden="true"></i>
+        <input type="text" id="first" value={second} onChange={(evt) => {updateSecond(evt.target.value)}}/>
+      </div>
+      <div className="total">
+        <p className="tvalue">
+          {Number(first) + Number(second)}
+        </p>
+      </div>
+      <button className="cal">
+        =
+      </button>
+    </div>
+  </div>
+  );
+}
