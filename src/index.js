2abbb90c343fe568fb54a36e15d10ea0bdfca4e5
Victor Nnamani <93020611+NeonVictor651@users.noreply.github.com>
2022-09-30 04:32:46 +0100

    no major changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
-import "./style.css";
+import JumiaPage2 from "./JumiaUI-P2/jumia-page2"
+import "./JumiaUI-P2/jumiaP2.css"
+// import "./style.css";
 // import { Calculator } from "./app";
-import Calculator2 from "./R-cal";
+// import Calculator2 from "./R-cal";
 // import CalculatorUI from "./mr-dogun's-cal-code";
+// import { JumiaLanding } from "./JumiaUI/jumia-main"
+// import "./JumiaUI/jumia.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Calculator2/>);
+root.render(<JumiaPage2/>);
